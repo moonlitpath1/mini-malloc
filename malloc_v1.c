f808ebe5e5db2950782730349d511c04df0bc7b0
Anushka Badhe <anushkabadhe@gmail.com>
2026-02-25 01:09:17 +0530

    Phase 1 - Creating Simple Malloc (Part 2)

--- malloc_v1.c
+++ malloc_v1.c
@@ -1,16 +1,21 @@
 //the most basic malloc ever
+//aka bump allocator -- since the ptr can only move forward like a bump on a counter --- your heap can only grow. never shrink. 
+//no memory reusability. 
+
 
 #include<unistd.h>
 #include<stddef.h>
 
 void* malloc(size_t size)
 {
-//	void *top = sbrk(0);
-//	printf("Print%p", top);
+	void *top = sbrk(0);
+	if(sbrk(size) == (void *)-1) return NULL;
+	return top;
 }
 
 void free(void *ptr)
 {
+	(void)ptr;
 	//intentionally empty for now -- we are going to test it on `ls` 
 	//but ls calls free() on memory, 
 	//if we dont provide the function the .so file has no free symbol,
@@ -20,8 +25,3 @@ void free(void *ptr)
 }
 
 
-int main()
-{
-
-	return 0;
-}
