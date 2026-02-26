0c56046f5ca691a6d8fd480630eb0dfb7f68c26d
Anushka Badhe <anushkabadhe@gmail.com>
2026-02-26 13:04:42 +0530

    Changes made to git push automation code

--- git_push.sh
+++ git_push.sh
@@ -5,9 +5,11 @@ set -e
 
 # 1) Copy files
 cp -r ~/anu/notes/🗺️Toy\ Malloc\ Journey/* ./notes/
+echo "Copied files successfully ✅"
 
 # 2) Add changes
 git add .
+echo "Changes staged for commit"
 
 # 3) Read commit message
 echo "Enter commit message:"
@@ -18,3 +20,4 @@ git commit -m "$message"
 
 # 5) Push
 git push
+echo "Files pushed successfully ✅"
